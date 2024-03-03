2e81f4180b9d86d361bec82a4ef65c4592519ef8
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-04 02:43:23 +0900

    style: change text color to red in datetime input with invalid scheduled time

--- components/publish/PublishWidget.vue
+++ components/publish/PublishWidget.vue
@@ -639,7 +639,7 @@ function stopQuestionMarkPropagation(e: KeyboardEvent) {
   color: var(--c-text-btn-disabled);
 }
 
-.option-input:focus+.delete-button {
+.option-input:focus + .delete-button {
   display: none;
 }
 
@@ -652,5 +652,9 @@ function stopQuestionMarkPropagation(e: KeyboardEvent) {
   justify-content: center;
   align-items: center;
   border-radius: 50%;
+}
+
+input[name="schedule-datetime"]:invalid {
+  color: var(--c-danger);
 }
 </style>
